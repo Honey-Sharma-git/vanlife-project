1c1e7992bec3ea2c51a0835546ef0f30199db05f
Honey-Sharma-git <honeysharmaemail@gmail.com>
2025-03-26 10:36:11 +0530

    Add underline hover class to links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,17 @@ export const Navbar = () => {
   return (
     <header className="bg-amber-50 flex justify-between items-center p-5">
       <h1 className="text-2xl font-bold ">
-        <Link to={"/"}>#VanLife</Link>
+        <Link className="hover:underline" to={"/"}>
+          #VanLife
+        </Link>
       </h1>
       <nav className="flex flex-row gap-5">
-        <Link to={"/about"}>About</Link>
-        <Link to={"/vans"}>Vans</Link>
+        <Link className="hover:underline" to={"/about"}>
+          About
+        </Link>
+        <Link className="hover:underline" to={"/vans"}>
+          Vans
+        </Link>
       </nav>
     </header>
   );
