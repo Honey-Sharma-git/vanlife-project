251b317b1cc8cf66b1afa0e30210b4ba9c50b29f
Honey-Sharma-git <honeysharmaemail@gmail.com>
2025-03-29 20:13:15 +0530

    Replaced <Link> with <Navlink> to show their active state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,41 @@
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 export const Header = () => {
   return (
     <header className="bg-amber-50 flex justify-between items-center p-5">
-      <h1 className="text-2xl font-bold ">
-        <Link className="hover:underline" to={"/"}>
-          #VanLife
-        </Link>
+      <h1 className="text-3xl font-bold ">
+        <Link to={"/"}>#VanLife</Link>
       </h1>
       <nav className="flex flex-row gap-5">
-        <Link className="hover:underline" to={"/about"}>
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? "text-amber-700 font-medium cursor-default"
+              : "text-black hover:underline "
+          }
+          to={"/about"}
+        >
           About
-        </Link>
-        <Link className="hover:underline" to={"/vans"}>
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? "text-amber-700 font-medium cursor-default"
+              : "text-black hover:underline "
+          }
+          to={"/vans"}
+        >
           Vans
-        </Link>
-        <Link className="hover:underline" to={"/host"}>
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? "text-amber-700 font-medium cursor-default"
+              : "text-black hover:underline "
+          }
+          to={"/host"}
+        >
           Host
-        </Link>
+        </NavLink>
       </nav>
     </header>
   );
