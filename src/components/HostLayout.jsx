7ffd46d82f2bdb60d97a66ef90874ae41f6085d7
Honey-Sharma-git <honeysharmaemail@gmail.com>
2025-03-29 20:35:58 +0530

    Add NavLinks to HostLayout with using end attribute on /host dashboard

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,13 +1,44 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 export const HostLayout = () => {
+  const activeStyles = {
+    fontWeight: "bold",
+    color: "#161616",
+    textDecoration: "underline",
+  };
+  const inactiveStyles = {};
   return (
     <>
       <nav className="flex flex-row rounded-2xl drop-shadow-2xl bg-amber-500 text-white gap-5 justify-between w-1/3 py-1 px-5">
-        <Link className="hover:underline" to={"/host"}> Dashboard </Link>
-        <Link className="hover:underline" to={"/host/income"}>Income </Link>
-        <Link className="hover:underline" to={"/host/reviews"}>Reviews </Link>
+        <NavLink
+          end={true}
+          style={({ isActive }) => {
+            return isActive ? activeStyles : null;
+          }}
+          to={"/host"}
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => {
+            return isActive ? activeStyles : null;
+          }}
+          to={"/host/income"}
+        >
+          Income
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => {
+            return isActive ? activeStyles : null;
+          }}
+          to={"/host/reviews"}
+        >
+          Reviews
+        </NavLink>
       </nav>
       <Outlet />
     </>
   );
 };
+
+{
+}
