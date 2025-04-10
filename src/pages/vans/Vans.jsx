04d15c1724ee87b27b2e6cd186c3c5954e02f7d8
Honey-Sharma-git <honeysharmaemail@gmail.com>
2025-04-10 15:10:22 +0530

    Add van filter

--- src/pages/vans/Vans.jsx
+++ src/pages/vans/Vans.jsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../../../server";
@@ -11,13 +12,22 @@ export async function getData(url) {
 
 export const Vans = () => {
   const [vans, setVans] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const typeFilter = searchParams.get("type");
+  console.log(typeFilter);
+
   useEffect(() => {
     getData(URL).then((res) => {
       setVans(res);
     });
   }, []);
 
-  const vansElements = vans.map((van) => {
+  const displayedVans = typeFilter
+    ? vans.filter((van) => {
+        return typeFilter === van.type;
+      })
+    : vans;
+  const vansElements = displayedVans.map((van) => {
     return (
       <Link
         to={`/vans/${van.id}`}
@@ -44,12 +54,55 @@ export const Vans = () => {
       </Link>
     );
   });
+
   return (
     <main>
       <div className="flex flex-col items-center">
         <h1 className=" text-4xl font-medium text-center">
           Explore our van options
         </h1>
+        <div className="flex flex-row justify-center gap-4">
+          <button
+            className={`bg-amber-200 cursor-pointer p-1 rounded-sm px-4 hover:bg-amber-500 ${
+              typeFilter === "simple" ? "simple" : null
+            }`}
+            onClick={() => {
+              setSearchParams({ type: "simple" });
+            }}
+          >
+            Simple
+          </button>
+          <button
+            className={`bg-amber-200 hover:text-white cursor-pointer p-1 rounded-sm px-4 hover:bg-black ${
+              typeFilter === "luxury" ? "luxury" : null
+            }`}
+            onClick={() => {
+              setSearchParams({ type: "luxury" });
+            }}
+          >
+            Luxury
+          </button>
+          <button
+            className={`bg-amber-200 hover:text-white cursor-pointer p-1 rounded-sm px-4 hover:bg-green-700 ${
+              typeFilter === "rugged" ? "rugged" : null
+            }`}
+            onClick={() => {
+              setSearchParams({ type: "rugged" });
+            }}
+          >
+            Rugged
+          </button>
+          {typeFilter && (
+            <button
+              className="bg-amber-200 cursor-pointer p-1 rounded-sm px-4 hover:bg-gray-200"
+              onClick={() => {
+                setSearchParams({});
+              }}
+            >
+              Clear
+            </button>
+          )}
+        </div>
         <article className="flex flex-row flex-wrap gap-5 justify-center p-5 w-[80%]">
           {vansElements}
         </article>
